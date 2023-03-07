766303485a33d32820a0417e4eef5969d3e95b5e
Billiam2468 <william.k.lee@wustl.edu>
2023-03-06 19:29:30 -0600

    updated blacklist to exclude all stickers

--- Self Contained Synthetic Data Generation/testContain.py
+++ Self Contained Synthetic Data Generation/testContain.py
@@ -1,14 +1,31 @@
-import sys, os
+# import sys, os
 
-scriptPath = os.path.realpath(__file__)
-scriptName = os.path.basename(__file__)
+# scriptPath = os.path.realpath(__file__)
+# scriptName = os.path.basename(__file__)
 
-print(scriptName)
-scriptPath = scriptPath.removesuffix(scriptName)
-print("renders would be at: ")
-print(scriptPath + "Renders/")
+# print(scriptName)
+# scriptPath = scriptPath.removesuffix(scriptName)
+# print("renders would be at: ")
+# print(scriptPath + "Renders/")
 
-with open(scriptPath + "blacklist.txt") as f:
-    if "u9577" in f.read():
-        print("test")
-    print("end of with statement")
+# with open(scriptPath + "blacklist.txt") as f:
+#     if "u9577" in f.read():
+#         print("test")
+#     print("end of with statement")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_html("https://library.ldraw.org/tracker/search?s=sticker&scope=header")
+
+print("total tables", len(table))
+
+df = table[1]
+print(df.head())
+
+for name in df['Part']:
+    name = name.removeprefix("parts/")
+    name = name.removeprefix("s/")
+    name = name.removesuffix(".dat")
+    print(name)
